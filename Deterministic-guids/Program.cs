83d412b1b48d0db895e9f66cf6a4d05b868ec2bd
Jamie Wroe <Jamie.Wroe@live.co.uk>
2017-04-06 11:53:02 +0100

    add guid generation

--- Deterministic-guids/Program.cs
+++ Deterministic-guids/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Deterministic_guids
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            while (true)
+            {
+                Console.WriteLine("Enter input to create a deterministic guid");
+                var input = Console.ReadLine();
+
+                using (var md5Hash = MD5.Create())
+                {
+                    var hash = GetHash(md5Hash, input);
+
+                    //MD5 gives us a 32 character hex string, which is the same as a guid!
+                    var guid = new Guid(hash);
+                    Console.WriteLine($"Detrministic guid is {guid}\n");
+                }
+
+            }
+        }
+
+        private static string GetHash(HashAlgorithm algo, string input)
+        {
+            var data = algo.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sBuilder = new StringBuilder();
+
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+
+            return sBuilder.ToString();
         }
     }
 }
